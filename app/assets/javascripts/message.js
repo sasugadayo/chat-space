25242623e55281f70f60c30d8f26858b716035a4
sasugadayo <sasugaff@icloud.com>
2020-01-26 14:02:48 +0900

    非同期で投稿内容をレスポンスする機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,6 @@
 $(function(){ 
+  last_message_id = $('.message:last').data("message-id");
+  console.log(last_message_id);
   function buildHTML(message){
     if ( message.image ) {
       var html =
@@ -48,6 +50,7 @@ $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
   var url = $(this).attr('action')
+  
   $.ajax({
     url: url,
     type: "POST",
