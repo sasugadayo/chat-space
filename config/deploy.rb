b69e88623ca1d641ebdfb7d1b048c0bfd60e26ba
sasugadayo <sasugaff@icloud.com>
2020-01-29 15:26:40 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,8 +25,18 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+set :default_env, {
+  rbenv_root: "/user/local/rbenv",
+  path: "/user/local/rbenv/shims:/user/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"]
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
+
 set :linked_files, %w{ config/secrets.yml }
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
